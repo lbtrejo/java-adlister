7a69f7bf765ee19879212f6d6b0d8bebe45f310e
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-03 09:50:56 -0600

    feat: servlets exercise complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,14 +6,32 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+    int counter = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String reset = request.getParameter("reset");
+
+        if (reset != null){
+            counter = 0;
+        }
+
+        counter++;
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        String name = request.getParameter("name");
+
+
+        if (name != null){
+            out.println("<h3>Hello " + name + "</h3>");
+        } else {
+            out.println("<h3>Hello World</h3>");
+        }
+
+        out.printf("<p>Page views: %d</p>", counter);
     }
 
 }
