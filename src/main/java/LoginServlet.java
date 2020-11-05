f89b58aeef57b9a2a22478ce3286d6183a3b9136
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-04 20:18:47 -0600

    feat: refactored login logic

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("admin")) {
+            request.getRequestDispatcher("/profile").forward(request, response);
+        } else {
+            response.sendRedirect("/login.jsp");
+        }
+    }
+
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+}
