76c1004e5d4555ceb327908465c8712ab9d416be
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-09 14:48:58 -0600

    feat: insert method functional, exercise complete

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 import com.mysql.cj.jdbc.Driver;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +50,20 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public Long insert(Ad ad) {
+        Statement statement = null;
+        String insertQuery = String.format("INSERT INTO ads (user_id, title, description) VALUES (%d, '%s', '%s')", ad.getUserId(), ad.getTitle(), ad.getDescription());
+
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()){
+                System.out.println("Inserted a new ad.  Ad ID: " + rs.getLong(1));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return null;
     }
 }
