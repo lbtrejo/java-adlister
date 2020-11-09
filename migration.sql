ee0023382e2260550db4f7f0f4b73b964d495f52
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-09 11:27:34 -0600

    feat: migration script complete

---
+++ migration.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS adlister_db;
+CREATE DATABASE adlister_db;
+USE adlister_db;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+                 ON DELETE CASCADE
+);
